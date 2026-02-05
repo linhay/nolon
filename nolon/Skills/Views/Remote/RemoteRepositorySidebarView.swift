1de7c0034c5a43451ddf4a751bf8363fe3ebd9f0
linhey <is.linhey@outlook.com>
2026-02-06 00:04:26 +0800

    Remove bottom add-repository button

--- nolon/Skills/Views/Remote/RemoteRepositorySidebarView.swift
+++ nolon/Skills/Views/Remote/RemoteRepositorySidebarView.swift
@@ -248,9 +248,6 @@ struct RemoteRepositorySidebarView: View {
             }
             .listStyle(.sidebar)
             .animation(.snappy(duration: 0.2), value: collapsedSectionIDs)
-            .safeAreaInset(edge: .bottom) {
-                addRepositoryButton
-            }
         }
         .overlay(alignment: .top) {
             syncHUDOverlay
